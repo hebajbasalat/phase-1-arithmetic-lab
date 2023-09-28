4dbf8f14cd51274c12d8796034d342e7988e9701
hebajbasalat <jaserheba7@gmail.com>
2023-09-28 13:29:04 +0300

    completed assignment1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 10;
+Number.isInteger(random);
+
+let num4 = 100;
+let num3 = 25;
+let mod = num4 / num3;
+
+let max;
+max = Math.max(10, 20);
